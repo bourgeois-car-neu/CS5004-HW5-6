c3b415d53af83934c00a2c40d2366bf70fa43708
cbourgeois <bourgeois.car@northeastern.edu>
2026-06-18 12:20:07 -0400

    Task 2: TestGameList.java #2 testAddToList()

--- src/test/java/TestGameList.java
+++ src/test/java/TestGameList.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 import student.GameList;
 import student.IGameList;
+import student.BoardGame;
+import java.util.stream.Stream;
 
 /**
  * JUnit test for the GameList class.
@@ -23,5 +25,14 @@ public class TestGameList {
     @Test
     public void testGetGameNamesEmpty() {
         assertEquals(List.of(), list.getGameNames());
+    }
+
+    @Test
+    public void testAddToListSingle() {
+        BoardGame game = new BoardGame("Chess", 0, 0, 0, 0, 0, 0.0, 0, 0.0, 0);
+        Stream<BoardGame> filtered = Stream.of(game);
+        list.addToList("Chess", filtered);
+        assertEquals(List.of("Chess"), list.getGameNames());
+
     }
 }
