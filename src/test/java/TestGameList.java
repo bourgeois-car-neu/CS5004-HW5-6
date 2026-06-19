31929053d3acd1ba4e0b8f0012703946c0dee0da
cbourgeois <bourgeois.car@northeastern.edu>
2026-06-19 13:48:17 -0400

    Task 2: TestGameList.java #3 testGetGameNames() 2.0 ascend alphabet order

--- src/test/java/TestGameList.java
+++ src/test/java/TestGameList.java
@@ -11,8 +11,9 @@ import java.util.stream.Stream;
  * JUnit test for the GameList class.
  */
 public class TestGameList {
-    /** Field for TestGameList. */
+    /** Field for GameList. */
     private IGameList list;
+
 
     @BeforeEach
     public void setup() {
@@ -27,6 +28,22 @@ public class TestGameList {
         assertEquals(List.of(), list.getGameNames());
     }
 
+    /**
+     * test getGameNames() for ascending alphabetical order.
+     */
+    @Test
+    public void testGetGameNamesAscendAlphaOrder() {
+        BoardGame game1 = new BoardGame("Monopoly", 1, 0, 0, 0, 0, 0.0, 0, 0.0, 0);
+        BoardGame game2 = new BoardGame("Chess", 2, 0, 0, 0, 0, 0.0, 0, 0.0, 0);
+        BoardGame game3 = new BoardGame("Azul", 3, 0, 0, 0, 0, 0.0, 0, 0.0, 0);
+        Stream<BoardGame> filtered = Stream.of(game1, game2, game3);
+        list.addToList("all", filtered);
+        assertEquals(List.of("Azul", "Chess", "Monopoly"), list.getGameNames());
+    }
+
+    /**
+     * test addToList() for a single name.
+     */
     @Test
     public void testAddToListSingle() {
         BoardGame game = new BoardGame("Chess", 0, 0, 0, 0, 0, 0.0, 0, 0.0, 0);
