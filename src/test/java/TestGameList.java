c879650686b948ab9a34857a67748b560821980a
cbourgeois <bourgeois.car@northeastern.edu>
2026-06-19 15:13:46 -0400

    Task 2: TestGameList.java #4 addToList() add by number

--- src/test/java/TestGameList.java
+++ src/test/java/TestGameList.java
@@ -30,6 +30,7 @@ public class TestGameList {
 
     /**
      * test getGameNames() for ascending alphabetical order.
+     * simultaneously tests addToList() for "all".
      */
     @Test
     public void testGetGameNamesAscendAlphaOrder() {
@@ -41,6 +42,7 @@ public class TestGameList {
         assertEquals(List.of("Azul", "Chess", "Monopoly"), list.getGameNames());
     }
 
+
     /**
      * test addToList() for a single name.
      */
@@ -50,6 +52,15 @@ public class TestGameList {
         Stream<BoardGame> filtered = Stream.of(game);
         list.addToList("Chess", filtered);
         assertEquals(List.of("Chess"), list.getGameNames());
+    }
 
+    @Test
+    public void testAddToListNumber() {
+        BoardGame game1 = new BoardGame("Monopoly", 1, 0, 0, 0, 0, 0.0, 0, 0.0, 0);
+        BoardGame game2 = new BoardGame("Chess", 2, 0, 0, 0, 0, 0.0, 0, 0.0, 0);
+        BoardGame game3 = new BoardGame("Azul", 3, 0, 0, 0, 0, 0.0, 0, 0.0, 0);
+        Stream<BoardGame> filtered = Stream.of(game1, game2, game3);
+        list.addToList("1", filtered);
+        assertEquals(List.of("Monopoly"), list.getGameNames());
     }
 }
